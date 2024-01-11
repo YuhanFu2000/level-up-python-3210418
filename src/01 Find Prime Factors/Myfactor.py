019a26cf7ec3d9b295e95e61f20ad16984c3ee01
YuhanFu2000 <120021465+YuhanFu2000@users.noreply.github.com>
2024-01-11 11:19:29 +0000

    Myfactor

---
+++ src/01 Find Prime Factors/Myfactor.py
@@ -0,0 +1,16 @@
+def get_prime_factors(number):
+  factors = []
+  divisor = 2
+  while divisor <= number:
+    if number % divisor == 0:
+      factors.append(divisor)
+      number //= divisor
+    else:
+      divisor += 1
+  return factors
+
+if __name__ == '__main__':
+  print(get_prime_factors(630))
+  print(get_prime_factors(13))
+
+
